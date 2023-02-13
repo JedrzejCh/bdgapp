8d4fe0f2f80df0118ae2865ab2e5f361fbf5e419
Jędrzej Choniawko <jchoniawko@gmail.com>
2023-02-13 21:19:11 +0000

    feat: frontend changes

--- frontend/src/components/molecules/test.vue
+++ frontend/src/components/molecules/test.vue
@@ -1,17 +1,18 @@
 <template>
-  <Switch v-model="enabled" :class="enabled ? 'bg-blue-600' : 'bg-gray-200'"
-    class="relative inline-flex h-6 w-11 items-center rounded-full">
-    <span class="sr-only">Enable notifications</span>
-    <span
-      :class="enabled ? 'translate-x-6' : 'translate-x-1'"
-      class="inline-block h-4 w-4 transform rounded-full bg-white transition"
-    />
-  </Switch>
+
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import { Switch } from '@headlessui/vue'
+<script lang="ts">
+import { ref } from 'vue';
+import { Switch } from '@headlessui/vue';
 
-const enabled = ref(false)
+const enabled = ref(true)
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+
 </script>
