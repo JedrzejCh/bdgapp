739aab37d1bc53365ec8141d8a80dbc099b45972
Jędrzej  Choniawko <jchoniawko@gmail.com>
2023-04-05 22:07:27 +0200

    front styles preprocessor scss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import Test from './components/molecules/test.vue';
+
 
 </script>
 
 <template>
-  <Test />
+ 
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
 </style>
